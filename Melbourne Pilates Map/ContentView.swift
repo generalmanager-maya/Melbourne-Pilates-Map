76535d878df3c278c16ee909c563468fa0f3bc15
generalmanager-maya <general.manager@mayahotmattowels.com>
2026-05-04 21:00:00 +1000

    Initial Commit

---
+++ Melbourne Pilates Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Melbourne Pilates Map
+//
+//  Created by Heather Haejoo Kim on 4/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Melbourne Pilates Map/Melbourne_Pilates_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Melbourne_Pilates_MapApp.swift
+//  Melbourne Pilates Map
+//
+//  Created by Heather Haejoo Kim on 4/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct Melbourne_Pilates_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
